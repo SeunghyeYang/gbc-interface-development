ba0f840472fff548c203abf3fab0eea94f59783a
SeunghyeYang <71297657+SeunghyeYang@users.noreply.github.com>
2020-10-26 20:48:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var jobTitle  = 'student';
+var geolocation  = 'Ontario';
+var salary = '30K-60K';
+var company = 'Rogers';
+
+document.body.innerHTML = `You will be a ${jobTitle} in ${geolocation}, making $${salary} for ${company}.`;
+console.log(future);
